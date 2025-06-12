0cffe599596144a770a87e60e33c08acaf60da32
JP <jplmarotinho22@gmail.com>
2025-06-12 11:19:58 -0300

    AuthorizationService, implementação do UserDetailsService

---
+++ src/main/java/com/tuorjp/login_jwt/service/AuthorizationService.java
@@ -0,0 +1,17 @@
+package com.tuorjp.login_jwt.service;
+
+import com.tuorjp.login_jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class AuthorizationService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
