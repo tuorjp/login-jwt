745bbc05a16d520c908c6e9dde72395b0a07f5dd
JP <jplmarotinho22@gmail.com>
2025-06-12 18:13:04 -0300

    authenticationManager adicionado em SecurityConfigurations, que pega a instância da classe que autentica

--- src/main/java/com/tuorjp/login_jwt/security/SecurityConfigurations.java
+++ src/main/java/com/tuorjp/login_jwt/security/SecurityConfigurations.java
@@ -3,6 +3,8 @@ package com.tuorjp.login_jwt.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,5 +26,10 @@ public class SecurityConfigurations {
                                 .anyRequest().authenticated()
                 )
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
