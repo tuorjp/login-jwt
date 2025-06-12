27c4a343fed6630779cf8d57bc7e6e998dfa9f89
JP <jplmarotinho22@gmail.com>
2025-06-12 18:42:37 -0300

    login e register com permitAll(), no futuro register vai ser permitido para admins somente

--- src/main/java/com/tuorjp/login_jwt/security/SecurityConfigurations.java
+++ src/main/java/com/tuorjp/login_jwt/security/SecurityConfigurations.java
@@ -24,6 +24,8 @@ public class SecurityConfigurations {
                 )
                 .authorizeHttpRequests(
                         authorize -> authorize
+                                .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
+                                .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/test").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
