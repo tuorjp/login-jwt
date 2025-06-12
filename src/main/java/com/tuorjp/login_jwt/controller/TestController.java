8737e453ad940d9a2ed8cd718c9571187ee0c696
JP <jplmarotinho22@gmail.com>
2025-06-12 17:57:18 -0300

    Post Method Test

--- src/main/java/com/tuorjp/login_jwt/controller/TestController.java
+++ src/main/java/com/tuorjp/login_jwt/controller/TestController.java
@@ -3,9 +3,7 @@ package com.tuorjp.login_jwt.controller;
 import com.tuorjp.login_jwt.model.Test;
 import com.tuorjp.login_jwt.repository.TestRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,11 @@ public class TestController {
     @GetMapping
     public List<Test> findAll() {
         return testRepository.findAll();
+    }
+
+    @PostMapping
+    public Test save(@RequestBody Test test) {
+        test.setId(null);
+        return testRepository.save(test);
     }
 }
