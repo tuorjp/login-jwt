7599aa2235e3b7f07701c5c6c688fb9e6fcc0859
JP <jplmarotinho22@gmail.com>
2025-06-12 17:57:49 -0300

    Filtros de segurança por requisição

--- src/main/java/com/tuorjp/login_jwt/security/SecurityConfigurations.java
+++ src/main/java/com/tuorjp/login_jwt/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.tuorjp.login_jwt.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,11 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers(HttpMethod.POST, "/test").hasRole("ADMIN")
+                                .anyRequest().authenticated()
                 )
                 .build();
     }
