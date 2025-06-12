3534f4fba286ac3d9aa13e7c543ba8a9ee6a395d
JP <jplmarotinho22@gmail.com>
2025-06-12 11:14:26 -0300

    repository com método findByLogin

---
+++ src/main/java/com/tuorjp/login_jwt/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tuorjp.login_jwt.repository;
+
+import com.tuorjp.login_jwt.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login);
+}
