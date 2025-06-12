b1b2a2515853c8002bb0cb42770efe3e385b5efa
JP <jplmarotinho22@gmail.com>
2025-06-12 11:11:35 -0300

    User e UserRole

---
+++ src/main/java/com/tuorjp/login_jwt/model/User.java
@@ -0,0 +1,52 @@
+package com.tuorjp.login_jwt.model;
+
+import com.tuorjp.login_jwt.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@Entity(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN) {
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_USER")
+            );
+        } else {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+}
