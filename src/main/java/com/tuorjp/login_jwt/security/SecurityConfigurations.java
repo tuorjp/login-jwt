99a3414f265436aa9a3a37ac3abf148733bbe075
JP <jplmarotinho22@gmail.com>
2025-06-12 18:17:53 -0300

    passwordEncoder() método que retorna o BCryptPasswordEncoder, que fornece métodos de criptografia

--- src/main/java/com/tuorjp/login_jwt/security/SecurityConfigurations.java
+++ src/main/java/com/tuorjp/login_jwt/security/SecurityConfigurations.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -31,5 +33,10 @@ public class SecurityConfigurations {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
