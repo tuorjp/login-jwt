8b405673264c6b84d28f403ed274a9eebf1bfe3b
JP <jplmarotinho22@gmail.com>
2025-06-14 14:54:27 -0300

    SecurityConfigurations: .addFilterBefore(), adicionando a classe SecurityFilter antes dos filtros de .authorizeHttpRequests(). SecurityFilter: classe que recupera o token do header "Bearer..." e salva no contexto da aplicação.

--- src/main/java/com/tuorjp/login_jwt/security/SecurityConfigurations.java
+++ src/main/java/com/tuorjp/login_jwt/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package com.tuorjp.login_jwt.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfigurations {
+    private final SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
@@ -24,11 +29,12 @@ public class SecurityConfigurations {
                 )
                 .authorizeHttpRequests(
                         authorize -> authorize
-                                .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
-                                .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/test").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class) // salva no contexto da aplicação, a autenticação do usuário
                 .build();
     }
 
