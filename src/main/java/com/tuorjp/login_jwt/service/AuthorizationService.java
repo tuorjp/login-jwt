7b79a79631c95187040f7da37e27e9dfee0795e0
JP <jplmarotinho22@gmail.com>
2025-06-14 14:52:26 -0300

    @Servide annotation, método para encontrar usuário pelo login

--- src/main/java/com/tuorjp/login_jwt/service/AuthorizationService.java
+++ src/main/java/com/tuorjp/login_jwt/service/AuthorizationService.java
@@ -5,13 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 public class AuthorizationService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return userRepository.findByLogin(username);
     }
 }
